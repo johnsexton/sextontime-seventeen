cc45cacf2eeb12247a8f96bf655b6b5b2ffceaf7
Sexton <SEXTONJ7@erau.edu>
2017-01-20 09:46:45 -0500

    duplicated content-homepage-list

--- content-list.php
+++ content-list.php
@@ -1,4 +1,11 @@
 <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <a href="<?php the_permalink(); ?>" class="entry-title" title="<?php echo 'Permalink to ' . the_title_attribute( 'echo=0' ); ?>" rel="bookmark"><?php the_title(); ?></a>
-    <div class="entry-date"><?php get_the_date() ?></div>
-</li><!-- #post-<?php the_ID(); ?> -->
+    <a href="<?php the_permalink(); ?>" class="preview-link" title="<?php echo 'Permalink to ' . the_title_attribute( 'echo=0' ); ?>" rel="bookmark">
+        <img src="<?php the_post_thumbnail_url('preview'); ?>"/>
+        <div class="meta">
+            <?php if ( get_post_type( get_the_ID() ) != 'projects' ) : ?>
+            <div class="date"><?php echo get_the_date(); ?></div>
+            <?php endif; ?>
+            <div class="preview-title"><?php the_title(); ?></div>
+        </div>
+    </a>
+</li>
