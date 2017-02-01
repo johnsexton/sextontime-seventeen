f84b90e906a9ab97170faa69c54eabfbae25c6e4
Sexton <SEXTONJ7@erau.edu>
2017-02-01 15:51:49 -0500

    updated links in the page

--- page-homepage.php
+++ page-homepage.php
@@ -14,7 +14,7 @@ get_header(); ?>
 <section class="latest section-content">
     <header class="section-header">
         <h1 class="section-title">Latest Works</h1>
-        In my work as a freelance web designer, instructional technologist/designer, I've produced many projects that I want to put on display! Take a look at my latest works below. Feel free to contact me about them if you have any questions. I can talk about this stuff all day! <a href="#">View all works</a>.
+        In my work as a freelance web designer, instructional technologist/designer, I've produced many projects that I want to put on display! Take a look at my latest works below. Feel free to contact me about them if you have any questions. I can talk about this stuff all day! <a href="<?php echo home_url( '/projects/' );?>">View all projects</a>.
     </header>
 
     <ul class="clearfix">
@@ -33,7 +33,7 @@ get_header(); ?>
 <section class="latest section-content">
     <header class="section-header">
         <h1 class="section-title">Latest Fishing Updates</h1>
-        Fishing is the greatest pastime a person can learn. I love it because it puts me in a wonderful state of mind. All the day-to-day distractions go away so there is nothing to worry about. There is no better feeling than the one you get the moment a fish grabs your onto bait and light tightens up and your fishing rod bends. In my fishing journal I like to share a lot of the pictures I take and maybe a story here and there. Check below for my latest fishing updates or <a href="#">view all my fishing entries</a>.
+        Fishing is the greatest pastime a person can learn. I love it because it puts me in a wonderful state of mind. All the day-to-day distractions go away so there is nothing to worry about. There is no better feeling than the one you get the moment a fish grabs your onto bait and light tightens up and your fishing rod bends. In my fishing journal I like to share a lot of the pictures I take and maybe a story here and there. Check below for my latest fishing updates or <a href="<?php echo home_url( '/fishing/' );?>">view all my fishing entries</a>.
     </header>
 
     <ul class="clearfix">
