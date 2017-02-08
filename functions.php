2c73661de23873a6afccaea515dd74f96d132541
Sexton <SEXTONJ7@erau.edu>
2017-02-08 10:37:51 -0500

    added wordpress title feature

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
 if ( ! function_exists( 'theme_setup' ) ) :
     // Add default posts and comments RSS feed links to head.
     add_theme_support( 'automatic-feed-links' );
+    // Let WordPress manage the document title.
+    add_theme_support( 'title-tag' );
     // Enable support for Post Thumbnails on posts and pages.
     add_theme_support( 'post-thumbnails' );
     set_post_thumbnail_size( 300, 0 );
