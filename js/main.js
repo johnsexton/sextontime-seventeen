23c65457ecf8eba08b418437d46b182170e0eb0a
Sexton <SEXTONJ7@erau.edu>
2017-03-09 15:03:56 -0500

    added support for fluid embedded youtube videos

--- js/main.js
+++ js/main.js
@@ -43,4 +43,33 @@ $(document).ready( function() {
         }
     );
 
+    $('iframe[src*="youtube.com"]').wrap('<div class="video-wrapper">').parent();
+
 });
+
+// Find all YouTube videos
+var $videos = $('iframe[src*="youtube.com"]'),
+    // The element that is fluid width
+    $body = $("body");
+
+// Figure out and save aspect ratio for each video
+$allVideos.each(function() {
+  $(this)
+    .data('aspectRatio', this.height / this.width)
+    // and remove the hard coded width/height
+    .removeAttr('height')
+    .removeAttr('width');
+});
+
+// When the window is resized
+$(window).resize(function() {
+    var newWidth = $body.width();
+    // Resize all videos according to their own aspect ratio
+    $videos.each(function() {
+        var $video = $(this);
+        $video
+        .width(newWidth)
+        .height(newWidth * $el.data('aspectRatio'));
+    });
+// Kick off one resize to fix all videos on page load
+}).resize();
